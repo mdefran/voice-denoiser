9f6c8d80e3795e8d5783c07888ff0a4351865e30
Matthew DeFranceschi <matthew.defranceschi@gmail.com>
2023-11-30 23:27:50 -0500

    initial unet implementation

--- test_file.py
+++ test_file.py
@@ -7,10 +7,9 @@ import numpy as np
 import os
 from scipy.io import wavfile
 from sklearn.model_selection import train_test_split
-import re
-# import tensorflow as tf
-# from tensorflow import keras
-# from keras import layers, models
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers, models
 
 # Function to read a .wav file into a spectrogram
 def wav_to_spectrogram(file_path, n_fft=2048, hop_length=512, save_phase=False):
@@ -50,12 +49,31 @@ def display_spectrogram(spectrogram, sr, hop_length, save_path):
 
 def load_data(clean_speech_folder, noisy_speech_folder, masks_folder, data_folder):
     db_interval = -60
+    max_height = 0
+    max_width = 0
 
+    # Find the dimensions of the largest image for padding
+    for clean_file_name in os.listdir(clean_speech_folder):
+        if clean_file_name.endswith(".wav"):
+            clean_speech_path = os.path.join(clean_speech_folder, clean_file_name)
+            clean_speech_spectrogram, _ = wav_to_spectrogram(clean_speech_path)
+            height, width = clean_speech_spectrogram.shape
+            max_height = max(max_height, height)
+            max_width = max(max_width, width)
+
+    # Iterate through source files
     for clean_file_name in os.listdir(clean_speech_folder):
         if clean_file_name.endswith(".wav"):
             # Load the clean speech file
             clean_speech_path = os.path.join(clean_speech_folder, clean_file_name)
             clean_speech_spectrogram, _ = wav_to_spectrogram(clean_speech_path)
+
+            # Calculate padding
+            pad_height = max_height - clean_speech_spectrogram.shape[0]
+            pad_width = max_width - clean_speech_spectrogram.shape[1]
+
+            # Add padding to the clean speech spectrogram
+            clean_speech_spectrogram = np.pad(clean_speech_spectrogram, ((0, pad_height), (0, pad_width)), mode='constant')
 
             # Create a binary mask from the clean speech file
             mask = (clean_speech_spectrogram > db_interval).astype(np.float32)
@@ -67,6 +85,9 @@ def load_data(clean_speech_folder, noisy_speech_folder, masks_folder, data_folde
                     noisy_speech_path = os.path.join(noisy_speech_folder, noisy_file_name)
                     noisy_speech_spectrogram, _ = wav_to_spectrogram(noisy_speech_path)
 
+                    # Add padding to the noisy speech spectrogram
+                    noisy_speech_spectrogram = np.pad(noisy_speech_spectrogram, ((0, pad_height), (0, pad_width)), mode='constant')
+
                     # Save the noisy spectrogram data
                     output_file_name = noisy_file_name.replace(".wav", "_spectrogram.png")
                     output_path = os.path.join(data_folder, output_file_name)
@@ -77,4 +98,33 @@ def load_data(clean_speech_folder, noisy_speech_folder, masks_folder, data_folde
                     output_path = os.path.join(masks_folder, output_file_name)
                     plt.imsave(output_path, mask, cmap='gray')
 
-load_data("CleanSpeech", "NoisySpeech", "Masks", "Data")
+    return max_width, max_height
+
+def unet_model(input_shape):
+    inputs = tf.keras.Input(shape=input_shape)
+    
+    # Define the encoder
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    # Define the decoder
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    up1 = layers.UpSampling2D(size=(2, 2))(conv2)
+
+    # Add more encoder-decoder blocks as needed
+
+    outputs = layers.Conv2D(1, 1, activation='softmax')(up1)
+    model = models.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+width, height = load_data("clean_speech", "noisy_speech", "masks", "data")
+data_train, data_val, mask_train, mask_val = train_test_split(
+    data, masks test_size=0.2, random_state=42
+)
+
+model = unet_model((width, height, 1))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit()
